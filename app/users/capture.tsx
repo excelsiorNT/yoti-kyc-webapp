6f13a7a09f7ade79faf2edec066ad1922bcfaac7
Nelson Tin Tam <tin.tam@builder.ai>
2025-07-30 11:28:32 +0100

    Update: capture/result page UI and API

--- app/users/capture.tsx
+++ app/users/capture.tsx
@@ -2,16 +2,19 @@ import FaceCapture from "@getyoti/react-face-capture"
 import {
   Box,
   Button,
-  TextField,
   Typography,
-  InputAdornment,
-  IconButton,
   Paper,
 } from "@mui/material";
+import { useLocalStorage } from "react-use";
+import { useNavigate } from "react-router";
 
 export function CaptureImage() {
-  const onSuccess = (payload: { img: string | any[]; }, base64PreviewImage: any) => { 
-    const response = fetch('/api/verify', {
+  const [user, setUser, removeUser] = useLocalStorage('user', '');
+  const [verified, setVerified, removeVerified] = useLocalStorage('verified', false);
+  const navigate = useNavigate();
+
+  const onSuccess = async (payload: { img: string; }, base64PreviewImage?: string) => { 
+    const response = await fetch('/api/verify', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -20,8 +23,16 @@ export function CaptureImage() {
         img: payload.img,
       }),
     })
-    console.log(response)
+    const data = await response.json();
+    setVerified(data.age.age_check === "pass" && data.antispoofing.prediction === "true")
+    navigate("/result", {state: { username: user, kycResult: data.age.age_check }});
+    console.log(data)
   };
+  const onBack = (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault();
+      // Handle go back logic here
+      window.history.back();
+    };
   const onError = (error: any) => console.log(error);
   return (
     <Box
@@ -31,20 +42,27 @@ export function CaptureImage() {
       minHeight="100vh"
       bgcolor="#f5f5f5"
     >
-      <Paper elevation={0} sx={{ p: 4, minWidth: 550, maxWidth: 650}}>
+      <Paper elevation={0} sx={{ p: 2, minWidth: 550, maxWidth: 550}}>
         <Typography variant="h5" mb={2} align="center">
           Account Verification
         </Typography>
         <Typography variant="caption" mb={2} align="center">
-          To ensure a safe and
-authentic community, it requires all new users to prove their age during registration.
+          To ensure a safe and authentic community, we require all new users to prove their age during registration.
         </Typography>
         <FaceCapture 
-        clientSdkId="fc0beb8f-b40d-42ce-b491-2ea5780c8c94" 
-        returnPreviewImage={true}
-        secure={false}
-        onSuccess={onSuccess} 
-        onError={onError} />
+          clientSdkId={import.meta.env.VITE_SDK_ID}
+          returnPreviewImage={true}
+          secure={false}
+          onSuccess={onSuccess} 
+          onError={onError} />
+        <Button
+          variant="outlined"
+          color="primary"
+          fullWidth
+          onClick={onBack}
+        >
+          Cancel
+        </Button>
       </Paper>
     </Box>
 
--- app/users/result.tsx
+++ app/users/result.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Box, Button, Paper, Typography } from "@mui/material";
 import { useLocalStorage } from "react-use";
 import { useNavigate } from "react-router";
@@ -12,11 +12,67 @@ export default function Result({
 }: ResultProps) {
   const [user, setUser, removeUser] = useLocalStorage('user', '');
   const [verified, setVerified, removeVerified] = useLocalStorage('verified', false);
+  const [session, setSession, removeSession] = useLocalStorage('session', '');
+
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  
   const navigate = useNavigate();
 
-  const onVerify = () => {
-    
+  useEffect(() => {
+    const fetchResult = async () => {
+      try {
+        const response = await fetch(`/api/verify?sessionId=${session}`, {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' }
+        });
+        const result = await response.json()
+        console.log(result)
+        if (result === "COMPLETE") {
+          setVerified(true);
+          removeSession();
+        }
+      } catch (error) {
+        console.error("API error:", error);
+      }
+    };
+
+    fetchResult(); // initial call
+
+    if (!verified && session) {
+      intervalRef.current = setInterval(fetchResult, 10000); // every 10 seconds
+    }
+
+    return () => {
+      if (intervalRef.current) clearInterval(intervalRef.current);
+
+    };
+  }, [verified]);
+
+  const onVerify = async () => {
+    if (session) {
+      window.location.href = `https://age.yoti.com/age-estimation?sessionId=${session}&sdkId=${import.meta.env.VITE_WEB_SDK_ID}`;
+    } else {
+      try {
+        const response = await fetch('/api/sessions', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            username: user
+          }),
+        })
+        const data = await response.json();
+        setSession(data.id)
+        console.log(data)
+        window.location.href = data.url
+      } catch (error) {
+        console.error("Error during verification:", error);
+        alert("Fetch URL failed. Please try again.");
+      }
+    }
   }
+  
   return (
     <Box
       display="flex"
@@ -25,7 +81,7 @@ export default function Result({
       minHeight="100vh"
       bgcolor="#f5f5f5"
     >
-      <Paper elevation={3} sx={{ p: 4, minWidth: 350 }}>
+      <Paper elevation={3} sx={{ p: 2, minWidth: 350 }}>
         <Typography variant="h5" mb={2} align="center">
           Verification Result
         </Typography>
@@ -37,14 +93,14 @@ export default function Result({
             <strong>KYC Result:</strong> { verified ? "Completed" : "Failed" }
           </Typography>
         </Box>
-        <Button
+        { !verified && <Button
           variant="contained"
           color="primary"
           fullWidth
           onClick={onVerify}
         >
           Verify Account with ID
-        </Button>
+        </Button>}
       </Paper>
     </Box>
   );
